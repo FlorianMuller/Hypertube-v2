0d07cdd2c1738c66de0daf66be9d372fdf19c0d4
psim42 <simphilippe42@yahoo.fr>
2020-05-08 03:36:36 +0200

    removed annoying useless import

--- client/components/Layout/Header.tsx
+++ client/components/Layout/Header.tsx
@@ -12,9 +12,6 @@ import {
   Hidden
 } from "@material-ui/core";
 import { Search, AccountCircle } from "@material-ui/icons";
-
-import SearchIcon from "@material-ui/icons/Search";
-
 import Cookies from "universal-cookie";
 import API from "../../util/api";
 import { useHeaderStyles } from "./Layout.styles";
